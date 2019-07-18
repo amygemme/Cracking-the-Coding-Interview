df0765f01b28c984885263df5345ed9834f82f44
amygemme81 <domathlikearockstar@gmail.com>
2019-07-18 14:11:54 -0700

    chapter 2 linked list begins

---
+++ Chapter1.js
@@ -0,0 +1,211 @@
+// ===============CRACK THE CODING INTERVIEW CHAPTER 1 =============// 
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {type: "input", message: "enter first string", name: "string1"},
+    {type: "input", message: "enter second string", name: "string2"}
+  ])
+  .then(answers => {
+    var input = answers;
+ 
+
+ let str1 = input.string1;
+ let str2 = input.string2;
+
+ console.log(str1 +" || " + str2);
+  
+//===========    1.1  are all char unique in a string   =======================
+
+console.log("(1.1) Is the string Unique")
+
+function uniqueChar(string) {
+    for (let i = 0; i < string.length; i++) {
+        if (i == string.search(string.charAt(i))) {
+            // console.log(string.search(string.charAt(i)));
+        }
+        else{return false;}
+    }
+
+    return true;
+   };
+   
+console.log(str1);
+console.log(uniqueChar(str1));
+console.log(str2 + "\n" + uniqueChar(str2));
+
+
+   //====  SOLVED ==== ///  
+
+    //  ============== 1.2  given two strings, is one a permutation of the other  ===========
+    console.log("(1.2) Is one string a permutation of the other")
+    
+    console.log("1: " + str1 + "\n2: " + str2);
+    var sortAlphabets = function(text) {
+        return text.split('').sort().join('');
+    };
+    
+    function IsPermOfOther(string1, string2) {
+        if(sortAlphabets(string1) == sortAlphabets(string2)){
+            return true;
+        }
+        else{return false}
+        };
+        
+    console.log(IsPermOfOther(str1, str2));
+
+    //  =========  1.3  replace all spaces with a '%20'   ===================
+   
+   console.log("1.3  replace all spaces with '%20'" )
+    function replaceSpace(str) {
+        let str1 = str.trim();
+        var chars = str1.split('');
+  
+        for (let i = 0; i < chars.length; i++) {
+          if (chars[i] == " ") {
+            chars[i] = "%20";
+          }
+        }
+        return chars.join('');
+      };
+  
+      console.log(replaceSpace(str1));
+      
+// ===========  1.4  is the string a permutaion of a palindrome   ==========
+    console.log("(1.4) is it a permutation of a palindrome")
+
+    let trim1 = str1.trim();
+    let trim2 = str2.trim();
+
+    function IsPermOfPal(trim) {
+        let newChar = 0;
+        let repeatChar = 0;
+        for (let i = 0; i < trim.length; i++) {
+            if (trim.charAt(i) != " ") {
+                if (trim.search(trim.charAt(i)) == i) {
+                    newChar++;
+                }
+                else {
+                    // ====================Working on the edge case for when a letter repeats 3 or more times 
+                    // ======================Such as "never odd or even" =======================
+
+                    // let edgeCase = trim.split(i);
+                    // console.log(trim.charAt(i));
+                    // console.log("edge:    " + edgeCase);
+                    repeatChar++;
+                }
+            }
+        }
+        // console.log("new" + newChar);
+        // console.log("repeat" + repeatChar);
+
+        if (newChar == repeatChar || newChar - 1 == repeatChar) { return true }
+        else { return false }
+
+
+    }
+
+    console.log("testing string 1" + "\n" + IsPermOfPal(trim1));
+    console.log("testing string 2" + "\n" + IsPermOfPal(trim2));
+
+
+///  ------  1.5  is the string one edit away? ------
+console.log("1.5  Is one string one edit away from the other? edits can be remove, replace or add a letter to a string")
+function OneEditAway(str1, str2){
+    let string1 = str1.trim();
+    let string2 = str2.trim();
+    let count = 0
+     if (string1.length === string2.length){
+      for(let i=0; i<string1.length; i++){
+        if(string1.charAt(i) == string2.charAt(i)){
+          count++;
+        }
+      }
+      if (count +1 == string1.length){
+        return true
+      }
+      else{return false}
+  }
+    else if(string1.length +1 == string2.length){
+      let longer = string2;
+      let shorter = string1;
+      for(let i=0; i<longer.length; i++){
+        if(shorter.charAt(i) != longer.charAt(i)){
+          for (let j=i; j< longer.length-i;j++){
+            if(shorter.charAt(j) != longer.charAt(j+1)){
+              console.log(shorter.charAt(j));
+              console.log(longer.charAt(j+1));
+              return false;
+            }
+            else {return true}
+          }
+          // console.log(longer);
+          // console.log(shorter);
+          
+        }
+      }
+    }
+    else if(string1.length -1 == string2.length){
+      let longer = string1;
+      let shorter = string2;
+      for(let i=0; i<longer.length; i++){
+        if(shorter.charAt(i) != longer.charAt(i)){
+          for (let j=i; j<= longer.length;j++){
+            if (j == longer.length) {return true}
+            else if(shorter.charAt(j) != longer.charAt(j+1)){
+              return false;
+            }
+            else {return true}
+          }
+          // console.log(longer);
+          // console.log(shorter);
+          
+        }
+      }
+    }
+    
+    else{return false}
+}
+  
+  console.log(OneEditAway(str1, str2));
+
+
+  //================= 1.7 rotate a matrix ===========
+
+  var a = [];
+  a.push(["1", "2", "3"]);
+  a.push(["4", "5", "6"]);
+  a.push(["7", "8", "9"]);
+
+  let n = a[0].length;
+
+
+  function rotate90(matrix) {
+    console.log("the original matrix is:")
+    display(matrix);
+      let b = [];
+      for (j = 0; j < n; j++) {
+        let temp = [];
+        for (i = n - 1; i >= 0; i--) {
+          temp.push(a[i][j])
+        }
+        b.push(temp);
+        // console.log(b);
+      }
+      console.log("The matrix rotated 90 degrees clockwise is:")
+      display(b);
+      return b;
+  }
+
+  rotate90(a);
+
+  function display(a) {
+    for (let i = 0; i < n; i++) {
+      console.log(a[i]);
+    }
+  }
+
+
+});
+
+
--- test.js
+++ test.js
@@ -2,11 +2,61 @@ const inquirer = require('inquirer');
 
 inquirer
   .prompt([
-    {type: "input", message: "enter first string", name: "string1"},
-    {type: "input", message: "enter second string", name: "string2"}
+    { type: "input", message: "enter first string", name: "string1" },
+    { type: "input", message: "enter second string", name: "string2" }
   ])
   .then(answers => {
-    console.log(answers) 
- });
+    var input = answers;
+    // console.log(input)
 
-  
+
+    let str1 = input.string1;
+    let str2 = input.string2;
+
+    // console.log(str1 + str2);
+    // test function 
+
+    var a = [];
+    a.push(["1", "2", "3"]);
+    a.push(["4", "5", "6"]);
+    a.push(["7", "8", "9"]);
+
+    let n = a[0].length;
+    console.log(n);
+    let even = true;
+    if (n % 2 == 0) { even = true }
+    else { even = false };
+    console.log("even  " + even);
+
+
+    function rotate90(matrix) {
+      display(matrix);
+        let b = [];
+        for (j = 0; j < n; j++) {
+          let temp = [];
+          for (i = n - 1; i >= 0; i--) {
+            console.log(a[i][j]);
+            console.log("i  " + i)
+            temp.push(a[i][j])
+          }
+          b.push(temp);
+          // console.log(b);
+          display(b);
+        }
+      
+    }
+
+    rotate90(a);
+
+    function display(a) {
+      for (let i = 0; i < n; i++) {
+        console.log(a[i]);
+      }
+    }
+
+
+
+    // function end
+
+  });
+
